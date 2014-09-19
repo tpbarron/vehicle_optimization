b6dceeca03b0e4a80a490e0ba3103a5b019842b8
Trevor Barron <barron.trevor@gmail.com>
2014-09-18 22:18:53 -0600

    #13 - In process road modeling. Adding intersection data types.

---
+++ src/road/Intersection.cpp
@@ -0,0 +1,18 @@
+/*
+ * Intersection.cpp
+ *
+ *  Created on: Sep 18, 2014
+ *      Author: trevor
+ */
+
+#include "Intersection.h"
+
+Intersection::Intersection() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Intersection::~Intersection() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/road/Intersection.h
@@ -0,0 +1,17 @@
+/*
+ * Intersection.h
+ *
+ *  Created on: Sep 18, 2014
+ *      Author: trevor
+ */
+
+#ifndef INTERSECTION_H_
+#define INTERSECTION_H_
+
+class Intersection {
+public:
+	Intersection();
+	virtual ~Intersection();
+};
+
+#endif /* INTERSECTION_H_ */
